789b167dd17bde6165814c5e52dd2f6c6591b90f
aayushk9 <ayushkokate1@gmail.com>
2025-02-15 19:15:12 +0530

    core add

--- server/app.js
+++ server/app.js
@@ -3,24 +3,27 @@ const express = require('express');
 const z = require("zod");
 const mongoose = require("mongoose");
 const todo = require("./db");
+const cors = require("cors");
 const URI = process.env.MONGO_URI
 
+
 mongoose.connect(URI)
-.then(() => {
-    console.log("DB connected");  
-})
-.catch((err) => {
-    console.error("DB connection error:", err);  
-});
+    .then(() => {
+        console.log("DB connected");
+    })
+    .catch((err) => {
+        console.error("DB connection error:", err);
+    });
 
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {  
+app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 })
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/getTodos", async (req, res) => {
     const title = req.body.title;
@@ -31,30 +34,29 @@ app.post("/getTodos", async (req, res) => {
         description: z.string(),
     })
 
-    const validateInput = verifyWithZod.safeParse({title, description});
+    const validateInput = verifyWithZod.safeParse({ title, description });
 
-    if(!validateInput.success){
-       return res.json({
-        msg: "please enter valid data"
-       })
+    if (!validateInput.success) {
+        return res.json({
+            msg: "please enter valid data"
+        })
     }
 
-    // put those todos(title and description at a place)
-    const todos = new todo({  
+    const todos = new todo({
         title: title,
         description: description,
         completed: false
-    })  
+    })
     await todos.save()
     return res.json({
         todos: validateInput
-    })  
+    })
 })
 
-app.get("/getTodos", async(req, res) => {
+app.get("/getTodos", async (req, res) => {
     const todos = await todo.find()
     return res.json({
-      todo: todos
+        todo: todos
     })
 })
 
@@ -62,26 +64,26 @@ app.put("/completed", async (req, res) => {
     const title = req.body.title;
     const description = req.body.description;
 
-    const verifyWithZod = z.object({
+    const verifyWithZod = z.object({  
         title: z.string(),
         description: z.string(),
     })
 
-    const validateInput = verifyWithZod.safeParse({title, description});
+    const validateInput = verifyWithZod.safeParse({ title, description });
 
-    if(!validateInput.success){
+    if (!validateInput.success) {
         return res.status(411).json({
             msg: "Please enter valid inputs"
         })
-    }  
+    }
 
-    await todo.updateOne({  
-      _id: req.body._id
+    await todo.updateOne({
+        _id: req.body._id
     }, {
         completed: true
     })
 
     res.json({
-       status: "Todo marked as done"
+        status: "Todo marked as done"
     })
 })
